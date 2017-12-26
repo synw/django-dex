79cda090ed0ed20692cff6a9ddcb811885d0a4b1
Win <synwe@yahoo.com>
2017-12-26 10:22:16 +0000

    Minor correction in imports in views

--- dex/views.py
+++ dex/views.py
@@ -3,7 +3,7 @@ from django.http.response import Http404
 from django.conf import settings
 from django.utils._os import safe_join
 TERM = "terminal" in settings.INSTALLED_APPS
-if TERM is True:
+if TERM is True and "django_downloadview" in settings.INSTALLED_APPS is True:
     from django_downloadview import PathDownloadView
 
 
